fd458339fd98b9d44153216c51b5bec29ff250af
Amit Mizrahi <amizra@outlook.com>
2014-02-15 09:48:58 -0500

    add multiple neural networks

--- mac/NeuralNetwork.js
+++ mac/NeuralNetwork.js
@@ -1,9 +1,21 @@
-var net = new brain.NeuralNetwork();
+var SAMPLING_RATE = 16;
+var nets = [];
+
+for(var i = 0; i < SAMPLING_RATE; i++) {
+	nets.push(new brain.NeuralNetwork());
+}
+
 var drive = require('./drive');
 
+function train(sampledData, char) {
+	for(var i = 0; i < SAMPLING_RATE; i++) {
+		nets[i].train([{input: sampleData[i], output: char.charCodeAt(0)}]);
+	}
+}
+
 function accept(sampledData) {
-	for(int i = 0; i < sampledData.length; i++) {
-		var out = net.run(sampledData[i]);
+	for(int i = 0; i < SAMPLING_RATE; i++) {
+		var out = nets[i].run(sampledData[i]);
 		drive.write(out);
 	}
 }
