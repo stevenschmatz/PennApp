aeabbdfc35ba797cb532145893903cd39ca33f94
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-15 00:39:32 -0500

    why isn't this working

--- pennapp_sketch/pennapp_sketch.ino
+++ pennapp_sketch/pennapp_sketch.ino
@@ -1,38 +1,18 @@
+int x;
+int y;
 
- //setup up accerleation variables
- int xAcc;
- int yAcc;
- int zAcc;
- 
- int zero = 282;//this was the zero point value for my accelerometer, your board may be slightly different.  see the comment below for calculating this with your own board.
 
-void setup(){
-  Serial.begin(9600);//set up serial
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+  pinMode(A5, INPUT);
 }
 
-void loop(){
-  
-  //read values
-  xAcc = analogRead(A0);
-  yAcc = analogRead(A1);
-  zAcc = analogRead(A2);
-  
-  /**
-  if you want to determine the zero point value of your board, 
-  comment out the last eight lines of this loop and uncomment the lines below.
-  Hold the board parallel to the ground and watch the serial monitor, 
-  the xAcc and yAcc values will hover around a common value, 
-  this is your "zero" value.  Change the value of the variable "zero" above.
-  **/
-  //print values
-  Serial.print("xAcc ");
-  Serial.println(xAcc);
-  Serial.print("yAcc ");
-  Serial.println(yAcc);
-  Serial.print("zAcc ");
-  Serial.println(zAcc);
-  Serial.println(" ");
-  delay(500);//wait
-
+void loop() {
+  y = analogRead(A5);
+  x = analogRead(A4);
+  Serial.print(y);
+  Serial.print("    ");
+  Serial.println(x);
+  delay(50);
 }
-
